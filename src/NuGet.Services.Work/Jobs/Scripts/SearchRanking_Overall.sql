7cb43af04cfeaf4ee2c8dfdaa00f1d0b36899da1
anurse <andrew@andrewnurse.net>
2014-03-13 10:36:40 -0700

    Fix NuGet/NuGetGallery#1963 by adding Updates to Search Ranking at a factor of 0.5

--- src/NuGet.Services.Work/Jobs/Scripts/SearchRanking_Overall.sql
+++ src/NuGet.Services.Work/Jobs/Scripts/SearchRanking_Overall.sql
@@ -1,4 +1,4 @@
-﻿SELECT TOP(200) Dimension_Package.PackageId, SUM(DownloadCount) 'Downloads'
+﻿SELECT TOP(200) Dimension_Package.PackageId, SUM(CASE WHEN Dimension_Operation.Operation = 'Install' THEN DownloadCount ELSE (0.5 * DownloadCount) END) 'Downloads'
 FROM Fact_Download
 INNER JOIN Dimension_Package ON Dimension_Package.Id = Fact_Download.Dimension_Package_Id
 INNER JOIN Dimension_Date ON Dimension_Date.Id = Fact_Download.Dimension_Date_Id
@@ -6,6 +6,6 @@ INNER JOIN Dimension_Operation ON Dimension_Operation.Id = Fact_Download.Dimensi
 WHERE Dimension_Date.[Date] >= CONVERT(DATE, DATEADD(day, -42, GETDATE()))
   AND Dimension_Date.[Date] < CONVERT(DATE, GETDATE())
   AND Dimension_Package.PackageListed = 1
-  AND Dimension_Operation.Operation = 'Install'
+  AND (Dimension_Operation.Operation = 'Install' OR Dimension_Operation.Operation = 'Update')
 GROUP BY Dimension_Package.PackageId
-ORDER BY SUM(DownloadCount) DESC
+ORDER BY Downloads DESC
