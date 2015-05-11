ac67f5ccbee7db30ea7f6043b36c7283a592fded
Xavier Decoster <xavier.decoster@outlook.com>
2015-05-11 20:16:45 +0200

    Update license header on files.

--- src/NuGet.Services.Work/Jobs/NuGetV2RepositoryMirrorerJob.cs
+++ src/NuGet.Services.Work/Jobs/NuGetV2RepositoryMirrorerJob.cs
@@ -1,4 +1,6 @@
-﻿extern alias NuGetCoreRef;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+extern alias NuGetCoreRef;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using Newtonsoft.Json.Linq;
